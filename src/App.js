c1f8d9eddbd1d067050b2e65ffe527466b2595df
joel@cornerstohn.com <joelpaulinraj99@gmail.com>
2025-01-16 14:17:12 +0530

    first-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './apps/home.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+    
+    
+  //route routes
+  
+  );
+}
+
+export default App;
